3ce8b40e92e04d50cce9c410754cb1be3b950776
luciussulla <maciek.kowalczyk@yandex.com>
2021-07-20 17:36:25 +0200

    Initial setup of components and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import pages 
+import Home from './pages/Home'
+// import About from './pages/About'
+import SingleCoctail from './pages/SingleCoctail'
+import Error from './pages/Error'
+// import Components
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>App component</h2>
     </div>
   );
 }
---
+++ src/components/Coctail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Coctail = ()=> {
+  return (
+    <div>
+      <h2>coctail component</h2>
+    </div>
+  )
+}
+
+export default Coctail
