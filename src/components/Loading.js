b9f644bd42b399c5676306e885d54a534c3f152e
luciussulla <maciek.kowalczyk@yandex.com>
2021-07-22 09:46:34 +0200

    Add spiner when searching for drinks

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = ()=> {
   return (
     <div className="loader">
-      <p>...Loading</p>
+      <div class="spinner"></div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../logo2.jpg'
+import { FaCocktail } from 'react-icons/fa';
 
 const Navbar = ()=> {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <Link to="/">
-          <img className="logo" src={logo} alt="coctail logo"/>
+          {/* <img className="logo" src={logo} alt="coctail logo"/> */}
+          <span className="drink-icon"><FaCocktail className="i-element"/></span>
         </Link>
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
